eedc78fc6a6a4f83f9f77f9f79d17f7259834aca
Aliaksei Kalesnikau <aliaksei.kalesnikau@sorted.com>
2019-04-19 13:47:52 +0300

    finishing server.js file, added common config

--- server.js
+++ server.js
@@ -7,3 +7,23 @@ const app = require('./middleware/app'),
   api = require('./middleware/api'),
   req = require('./middleware/req'),
   err = require('./middleware/err');
+
+const config = require('./config/common');
+
+const server = express();
+
+server.engine('html', cons.mustache);
+server.set('view engine', 'html');
+
+server.use(helmet());
+server.use(compress({ threshold: 0 }));
+server.use(express.static('dist'));
+
+server.use(req());
+
+server.all('/api/*', api());
+
+server.use(app());
+server.use(err());
+
+server.listen(config.port);
