1f4bfaba47619c40e605cfe25252e27f4ae47172
Aliaksei Kalesnikau <aliaksei.kalesnikau@sorted.com>
2019-04-19 14:05:53 +0300

    first middleware was added

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = require('./middleware/app'),
   req = require('./middleware/req'),
   err = require('./middleware/err');
 
+const methodOverride = require('method-override');
+
 const config = require('./config/common');
 
 const server = express();
@@ -16,6 +18,15 @@ server.engine('html', cons.mustache);
 server.set('view engine', 'html');
 
 server.use(helmet());
+
+// Так как мы планируем работать не только с GET-запросами, но и со всеми возможными REST-глаголами (POST/PUT/DELETE),
+// а также выполнять запросы без JS на клиенте (т.е. по средствам html-форм), необходимо также внести парочку
+// изменений в основной файл веб-сервера из первой части
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(methodOverride('_method'));
+
+
 server.use(compress({ threshold: 0 }));
 server.use(express.static('dist'));
 
@@ -25,5 +36,7 @@ server.all('/api/*', api());
 
 server.use(app());
 server.use(err());
+
+
 
 server.listen(config.port);
